d7471aae88dc80205bd11c038fef74a0cdd99117
nmaslenko <nvmaslenko@gmail.com>
2021-11-05 13:21:32 +0500

    добавлена логика в работу функций: IsPrime(), IsOdd

--- numberParams.go
+++ numberParams.go
@@ -1,13 +1,22 @@
 package NumberParams
 
-func IsPrime(i int) bool {
-	return true
+func IsPrime(n int) bool {
+	var flag bool
+	if n < 2 {
+		flag = false
+	} else if n == 2 {
+		flag = true
+	} else {
+		for i := 2; i < n/2; i++ {
+			if n%2 == 0 {
+				flag = false
+				break
+			}
+		}
+	}
+	return flag
 }
 
-func IsOdd(i int) bool {
-	return true
-}
-
-func IsSquare(i int) bool{
-	return true
+func IsOdd(n int) bool {
+	return n&2 != 0
 }
