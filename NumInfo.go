f5a0813078898fa0849a409efb07b50f61e4090f
nmaslenko <nvmaslenko@gmail.com>
2021-11-05 21:06:01 +0500

    add new function GetMaxDiliter

--- NumInfo.go
+++ NumInfo.go
@@ -1,5 +1,7 @@
 package NumInfo
 
+import "errors"
+
 // IsPrime func get int number (n) and returned bool value: true - if n is prime number, and false - if n is not prime number
 func IsPrime(n int) bool {
 	if n < 2 {
@@ -24,4 +26,25 @@ func IsOdd(n int) bool {
 // IsBigger func get two int numbers, and returned true if n1 > n2, else - false
 func IsBigger(n1, n2 int) bool {
 	return n1 > n2
+}
+
+// GetMaxDivider func get int type number, and returned bigger divider
+func GetMaxDivider(n int) (int, error) {
+	if n == 0 {
+		err := errors.New("zero number has not divider")
+		return 0, err
+	} else if n > 1 {
+		for i := n - 1; i > 0; i-- {
+			if n%i == 0 {
+				return i, nil
+			}
+		}
+	} else {
+		for i := n + 1; i < 0; i++ {
+			if n%i == 0 {
+				return i, nil
+			}
+		}
+	}
+	return n, nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/nviktorovich/NumInfo
+module github.com/nviktorovich/NumInfo/v2
 
 go 1.17
